e2c1be91be73f8265b9715864691f22b901c6228
RiccardoColella <colella.git@outlook.com>
2022-09-29 14:36:54 +0200

    [AUTO] Periodical repo update

---
+++ AMA-win/autoeditor.ps1
@@ -0,0 +1,20 @@
+Set-Location $PSScriptRoot
+
+git pull
+
+$filePath = ".\uselessFile.txt"
+
+$status = (git status)
+if ($status -contains "nothing to commit, working tree clean"){
+    Write-Output "No changes in the repo. Creating mock modification."
+    Add-Content $filePath ("Line N. " + ([int]((Get-Item -Path $filePath | Get-Content -Tail 1) -replace '[^0-9]' , '') + 1))
+} else {
+    Write-Output "Already some changes are present in the folder"
+}
+$logs = Join-Path "$PSScriptRoot" ".\logs.txt"
+$status > $logs
+
+git add *
+git status
+git commit -m "[AUTO] Periodical repo update" -S
+git push origin master
