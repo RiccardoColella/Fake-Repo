5bc51c4930973ed19fad53468cbbdaaa37309c4d
RiccardoColella <colella.git@outlook.com>
2022-11-02 10:25:42 +0100

    [AUTO] Periodical repo update AMA-win

--- ENGWords/pyScript/findWords.py
+++ ENGWords/pyScript/findWords.py
@@ -1,10 +1,15 @@
 wordList = []
 
-needed_chars    = ['d']
-forbidden_chars = ['l', 'i', 'b', 'w', 'o', 'm', 'n', 't', 'a', 'c', 's', 'f', 'z']
+needed_chars    = ['i', 'r', 'e']
+forbidden_chars = ['g', 'a', 't','n', 'd', 'x']
+chars_not_first = ['i']
+chars_not_second = ['r']
+chars_not_third = ['e']
+chars_not_fourth = []
+chars_not_fifth = []
 first   = '?'
-second  = 'u'
-third   = 'd'
+second  = 'l'
+third   = 'i'
 fourth  = 'e'
 fifth   = 'r'
 
@@ -22,6 +27,12 @@ def filter_letter_position(word, letter, position):
         return True
     else:
         return False
+
+def filter_letter_not_position(word, letters, position):
+    for l in letters:
+        if (word[position-1] == l):
+            return False
+    return True
   
 def filter_first(word):
     return filter_letter_position(word, first, 1)
@@ -37,6 +48,21 @@ def filter_fourth(word):
 
 def filter_fifth(word):
     return filter_letter_position(word, fifth, 5)
+  
+def filter_not_first(word):
+    return filter_letter_not_position(word, chars_not_first, 1)
+
+def filter_not_second(word):
+    return filter_letter_not_position(word, chars_not_second, 2)
+
+def filter_not_third(word):
+    return filter_letter_not_position(word, chars_not_third, 3)
+
+def filter_not_fourth(word):
+    return filter_letter_not_position(word, chars_not_fourth, 4)
+
+def filter_not_fifth(word):
+    return filter_letter_not_position(word, chars_not_fifth, 5)
 
 def must_contain(word):
     for c in needed_chars:
@@ -58,6 +84,11 @@ filtered = filter(filter_second, filtered)
 filtered = filter(filter_third, filtered)
 filtered = filter(filter_fourth, filtered)
 filtered = filter(filter_fifth, filtered)
+filtered = filter(filter_not_first, filtered)
+filtered = filter(filter_not_second, filtered)
+filtered = filter(filter_not_third, filtered)
+filtered = filter(filter_not_fourth, filtered)
+filtered = filter(filter_not_fifth, filtered)
 filtered = filter(must_contain, filtered)
 filtered = filter(not_contains, filtered)
 
