89c5157005b5652d66e254eaaa593b4726300eda
Riccardo Colella <rcolella@brainloop.com>
2023-05-09 19:03:45 +0200

    [Mousemover] Update to a working solutions

--- WinMouseMover/mousemover.ps1
+++ WinMouseMover/mousemover.ps1
@@ -1,12 +1,7 @@
-Add-Type -AssemblyName System.Windows.Forms
-
-$backOrForth = $true
+$shell = New-Object -ComObject WScript.Shell
 
 while ($true)
 {
-  $pos = [System.Windows.Forms.Cursor]::Position
-  $delta = If ($backOrForth) {1} Else {-1}
-  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(($pos.X + $delta), ($pos.Y + $delta))
-  $backOrForth = -Not $backOrForth
-  Start-Sleep -Milliseconds 200
+  $shell.SendKeys('+{F15}')
+  Start-Sleep -seconds 59
 }
